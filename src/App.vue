5d874cd22d718ee13403372dfd1a838b044fe698
EastWind <linxiayoudongfeng@gmail.com>
2021-07-29 22:15:49 +0800

    '测试pre-commit'

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-07-29 21:34:57
+ * @LastEditTime: 2021-07-29 22:15:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue3-ts-cms\src\App.vue
+-->
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
     HelloWorld,
-  },
-});
+  }
+})
 </script>
 
 <style lang="less">
