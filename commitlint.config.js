8f504da7757c7a942c02abdc93bdf6643241bc75
EastWind <linxiayoudongfeng@gmail.com>
2021-07-29 22:35:13 +0800

    '.'

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-29 22:33:51
+ * @LastEditTime: 2021-07-29 22:34:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue3-ts-cms\commitlint.config.js
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+}
